8b46eea06cfcb50b3d935038b0a2ca50ec357891
youba idjis <youba.etud@gmail.com>
2023-08-01 23:51:36 -0700

    project setup

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,72 @@
-import React from 'react'
+import React from 'react';
+import { useEffect, useRef } from 'react';
+import logo from '../../assets/images/logo-no-background.png';
+import { NavLink, Link } from 'react-router-dom';
+
+const navLinks = [
+  {
+    path: '/home',
+    display: 'Home'
+  },
+  {
+    path: '/users',
+    display: 'Find a flight'
+  },
+  {
+    path: '/services',
+    display: 'Services'
+  },
+  {
+    path: '/contact',
+    display: 'Contact'
+  }
+]
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='header flex items-center'>
+      <div className="container">
+        <div className='flex items-center justify-between'>
+          { /* ======= Logo ========= */}
+          <div >
+            <img
+              width={100}
+              height={70}
+              src={logo}
+              alt='logo'
+            />
+          </div>
+
+          {/* ========= Menu ========= */}
+          <div className='navigation'>
+            <ul className="menu flex items-center gap-[2.7rem]">
+              {
+                navLinks.map((link, index) => <li key={index}>
+                  <NavLink to={link.path}
+                    className={navClass => navClass.isActive ? 'text-primaryColor text-[16px] leading-7 font-[600]'
+                      : 'text-textColor text-[16px] leading-7 font-[500] hover:text-primaryColor'}>{link.display} </NavLink>
+                </li>)
+              }
+            </ul>
+          </div>
+
+          {/*========= Nav right ====== */}
+          <div className='flex items-center gap-4'>
+
+            <div>
+              <Link to='/'>
+                  <figure>
+                    <img src='' alt='' />
+                  </figure>
+              </Link>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+
+    </header>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,21 @@
-module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      extend: {
-        buttonBgcolor: "#0067FF",
+    extend: {
+      colors: {
+        primaryColor: "#0067FF",
         yellowColor: "#FEB60D",
-        purpleColor: "#9771ff",
-        irisBlueColor: "01B5C5",
+        purpleColor: "#9771FF",
+        irisBlueColor: "#01B5C5",
         headingColor: "#181A1E",
         textColor: "#4E545F",
       },
+
       boxShadow: {
         panelShadow: "rgba(17, 12, 46, 0.15) 0px 48px 100px 0px;",
-      }
+      },
     },
   },
-  variants: {
-    extend: {},
-  },
   plugins: [],
-}
+};
